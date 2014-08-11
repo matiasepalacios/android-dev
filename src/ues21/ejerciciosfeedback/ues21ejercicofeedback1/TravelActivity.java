cb561fa056ed3da15e15ce63ed85fe9eddc82ca7
Matias Palacios <matias@praxisis.com>
2014-08-10 21:25:42 -0300

    adding more classes

---
+++ src/ues21/ejerciciosfeedback/ues21ejercicofeedback1/TravelActivity.java
@@ -0,0 +1,13 @@
+package ues21.ejerciciosfeedback.ues21ejercicofeedback1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TravelActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_travel);
+	}
+}
